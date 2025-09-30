bd0e9ce474744fb38415af84e45cc918cdc96e8f
Mayank <maynkaw@email.com>
2025-10-01 00:27:56 +0530

    V1

---
+++ firstRecursion/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    private final ThreadLocal<Integer> n = ThreadLocal.withInitial(() -> 0);
+    public Main(int n){
+        this.n.set(n);
+    }
+    public  void printNos(int number) {
+        if(number==0) return;
+        printNos(number-1);
+        System.out.print(number+" ");
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        if(sc.hasNextInt()){
+            int input=sc.nextInt();
+            Main obj=new Main(input);
+            obj.printNos(obj.n.get());
+        }else
+            System.out.println("Invalid input please enter integer");
+        System.out.println();
+
+    }
+}
